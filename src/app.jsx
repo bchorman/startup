5adffd321b135ab0849d7262b1a57f31f07f6ced
bchorman <brett.horman@gmail.com>
2025-02-12 11:47:46 -0700

    navigation works

--- src/app.jsx
+++ src/app.jsx
@@ -1,39 +1,58 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { About } from './about/about';
+import { Chat } from './chat/chat';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Profile } from './profile/profile';
 
 export default function App() {
-  return <div className="body bg-dark text-light">
-    <header class="container-fluid">
-            <nav class="navbar fixed-top navbar-dark">
-                <a class="navbar-brand" href="">WeatherChat</a>
-                <menu class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="chat.html">Chat</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="profile.html">Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.html">About</a>
-                    </li>
-                    <li id="login-button" class="btn btn-primary btn-sm ms-auto me-3">
-                        <a class="nav-link" href="login.html">Login</a>
-                    </li>
-                </menu>
-            </nav>
-        </header>
+  return <div classNameName="body bg-dark text-light">
+            <BrowserRouter>
+                <header className="container-fluid">
+                    <nav className="navbar fixed-top navbar-dark">
+                        <a className="navbar-brand" href="">WeatherChat</a>
+                        <menu className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="home">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="chat">Chat</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="profile">Profile</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="about">About</NavLink>
+                            </li>
+                            <li id="login-button" className="btn btn-primary btn-sm ms-auto me-3">
+                                <NavLink className="nav-link" to="login">Login</NavLink>
+                            </li>
+                        </menu>
+                    </nav>
+                </header>
 
-        <main>App components go here</main>
+                <Routes>
+                    <Route path='/' element={<Home />} exact />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/chat' element={<Chat />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/profile' element={<Profile />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
 
-        <footer class="bg-dark text-white-50">
-            <div class="container-fluid">
-                <span class="text-reset">Brett Horman</span>
-                <a class="text-reset" href="https://github.com/bchorman/startup">GitHub</a>
-            </div>
-        </footer>
-  </div>;
+                <footer className="bg-dark text-white-50">
+                    <div className="container-fluid">
+                        <span className="text-reset">Brett Horman</span>
+                        <a className="text-reset" href="https://github.com/bchorman/startup">GitHub</a>
+                    </div>
+                </footer>
+            </BrowserRouter>
+        </div>;
 }
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
